d10fbf16d5392aac9d459affcf03f67fe500c5bf
Ville Salonen <ville.salonen@iki.fi>
2013-10-03 20:33:08 +0300

    Country can now be passed as an argument

--- switch.js
+++ switch.js
@@ -7,14 +7,34 @@ var email = config[0];
 var password = config[1];
 var userId = config[2];
 
-casper.start('http://www.unotelly.com/quickstart2/login.php', function() {
-    this.fill('form', {
-        'email':      email,
-        'password2':  password
-    }, true);
-});
+var countries = {
+    'usa': 1,
+    'can': 2,
+    'gbr': 3,
+    'irl': 5,
+    'bra': 6,
+    'mex': 8,
+    'swe': 9,
+    'nor': 18,
+    'dnk': 19,
+    'fin': 20,
+    'nld': 27,
+};
 
-casper.thenOpen('http://quickstart3.unotelly.com/user/' + userId + '/dynamo', function () {
+if (casper.cli.args.length !== 1) {
+    console.log('Usage: casperjs switch.js CAN');
+    casper.exit();
+}
+
+var countryName = casper.cli.args[0].toLowerCase();
+var countryCode = countries[countryName];
+
+if (typeof countryCode === 'undefined') {
+    console.log('Invalid country name ' + countryName + '!');
+    casper.exit();
+}
+
+casper.start('http://quickstart3.unotelly.com/login', function() {
     this.fill('form', {
         'email':      email,
         'password':   password
@@ -23,12 +43,10 @@ casper.thenOpen('http://quickstart3.unotelly.com/user/' + userId + '/dynamo', fu
 
 casper.thenOpen('http://quickstart3.unotelly.com/user/' + userId + '/dynamo', function () {
     this.fill('form', {
-        '4':      '2'
+        '4':      countryCode + ''
     });
-});
-
-casper.then(function () {
-   this.capture('big-google.png');
+    
+    console.log("Switched to " + countryName.toUpperCase() + ".");
 });
 
 casper.run();
